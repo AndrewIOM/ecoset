fbb098bd67f6acbe8e6fd87da59cabc27218e0b5
AndrewIOM <AndrewIOM@users.noreply.github.com>
2023-11-19 21:43:02 +0000

    Add basic error handling to api

--- src/api/app.ts
+++ src/api/app.ts
@@ -3,7 +3,12 @@ import { ExpressAdapter } from '@bull-board/express';
 import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
 import bodyParser = require('body-parser');
 import config from 'config';
-import express from 'express';
+import express, {
+  Response as ExResponse,
+  Request as ExRequest,
+  NextFunction,
+} from "express";
+import { ValidateError } from "tsoa";
 import swaggerUi from 'swagger-ui-express';
 import { logger } from './logger';
 import { RegisterRoutes } from './routes/routes';
@@ -27,6 +32,28 @@ const app = express();
 app.use(bodyParser.json());
 
 RegisterRoutes(app);
+
+app.use(function errorHandler(
+  err: unknown,
+  req: ExRequest,
+  res: ExResponse,
+  next: NextFunction
+): ExResponse | void {
+  if (err instanceof ValidateError) {
+    console.warn(`Caught Validation Error for ${req.path}:`, err.fields);
+    return res.status(422).json({
+      message: "Validation Failed",
+      details: err?.fields,
+    });
+  }
+  if (err instanceof Error) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+
+  next();
+});
 
 app.use('/admin/queues', serverAdapter.getRouter());
 app.use('/', swaggerUi.serve as any, swaggerUi.setup(x) as any);
